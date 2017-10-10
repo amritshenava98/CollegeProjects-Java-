a6d66f3731110bcbeac2c80d95bf3dea0829aba8
Amrit Shenava <amritshenava98@users.noreply.github.com>
2017-10-10 10:24:34 +0400

    Create BasicArithmetic.java

---
+++ BasicArithmetic.java
@@ -0,0 +1,26 @@
+public class BasicArithmetic
+{
+ public static void main(String[] args)
+ {
+  int number1 = 11;
+  int number2 = 5;
+
+  int sum = number1 + number2;
+
+  int product = number1 * number2;
+
+  int difference = number1 - number2;
+
+  double d = (double)number1/(double)number2;
+
+  System.out.println("First Number : "+number1);
+  System.out.println("Second Number : "+number2);
+  
+  System.out.println("The sum of the two numbers is : "+sum);
+  System.out.println("The product of the two numbers is : "+product);
+  System.out.println("The difference between the two numbers is : "+difference);
+  System.out.println("Division : "+d);
+ 
+ }
+
+}
