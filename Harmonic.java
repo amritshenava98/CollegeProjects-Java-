cdd13d1c7f9a6bf99feec8124a9f9511e5517edd
Amrit Shenava <amritshenava98@users.noreply.github.com>
2017-10-29 13:32:07 +0400

    Update Harmonic.java

--- Harmonic.java
+++ Harmonic.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+
+public class Harmonic
+{
+ public static void main(String args[])
+ {
+  
+  int n, i=1;
+  double sum=0;
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the number for length of series : ");
+  n = sc.nextInt();
+
+  while(i <= n)
+  {
+   System.out.print(" 1/"+i+" +");
+   sum=sum+(double)1/i;
+   i++;
+  }
+ System.out.print("\nSum = "+sum);
+ }
+}
+ 
